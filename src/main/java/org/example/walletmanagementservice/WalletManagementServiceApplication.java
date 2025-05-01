d7121e61a806609461af0b13fa7f8ec2372ef9bb
Vladimir Stolyarov <laserjet4l.mgn@gmail.com>
2025-05-01 17:36:40 +0500

    First commit

---
+++ src/main/java/org/example/walletmanagementservice/WalletManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.walletmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/walletmanagementservice/WalletManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.walletmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
